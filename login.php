dc6e4f002d56534d96ee88dce0f4679f6cdcfe46
Yasmim Cosme <yasmimcosm@gmail.com>
2026-09-07 11:47:33 -0300

    fix: corrige sistema de login

--- login.php
+++ login.php
@@ -2,13 +2,82 @@
 
 session_start();
 
-//Remove os dados armazenados na sessão
-session_unset();
-//Destrói a sessão no servidor.
-session_destroy();
+require 'db.php';
 
-http_response_code(200);
+ $input = json_decode(file_get_contents("php://input"), true);
 
-echo json_encode([
-    'message' => 'Logout realizado com sucesso.'
+if (
+    !isset($input['email']) ||
+    !isset($input['password'])
+) {
+    http_response_code(400); // 400 Bad Request - Dados inválidos
+
+    echo json_encode([
+        'message' => 'Todos os campos sao obrigatorios.'
+    ]);
+
+    exit;
+}
+
+$email = $input['email'];
+$password = $input['password'];
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    http_response_code(400); // 400 Bad Request - Dados inválidos
+
+    echo json_encode([
+        'message' => 'E-mail inválido.'
+    ]);
+
+    exit;
+}
+
+if (strlen($password) < 8) {
+    http_response_code(400); // 400 Bad Request - Dados inválidos
+
+    echo json_encode([
+        'message' => 'Senha inválida. A senha deve ter pelo menos 8 caracteres.'
+    ]);
+
+
+    exit;
+}
+
+$stmt = $pdo->prepare(
+    "SELECT id, password_hash
+     FROM users
+     WHERE email = :email"
+);
+
+$stmt->execute([
+    'email' => $email
 ]);
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$user) {
+    http_response_code(401); // Unauthorized = as credenciais não permitiram autenticação.
+
+    echo json_encode([
+        'message' => 'Credenciais inválidas.'
+    ]);
+
+    exit;
+}
+
+if (password_verify($password, $user['password_hash'])) {
+
+    session_regenerate_id(true);
+
+    $_SESSION['user_id'] = $user['id'];
+
+    echo "Login realizado!";
+
+} else {
+
+    http_response_code(401);
+
+    echo json_encode([
+        'message' => 'Credenciais inválidas.'
+    ]);
+}
