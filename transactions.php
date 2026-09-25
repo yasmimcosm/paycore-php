bcd5a2135c0a33afd78ae72b287ee2961f8affe7
Yasmim Cosme <yasmimcosm@gmail.com>
2026-09-25 15:54:18 -0300

    feat: implementar CRUD de transações

--- transactions.php
+++ transactions.php
@@ -4,128 +4,326 @@ require 'db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-if($method == 'POST'){
-    $input = json_decode(file_get_contents("php://input"), true);
+switch ($method) {
+    case 'POST':
+        $input = json_decode(file_get_contents("php://input"), true);
 
-    if (
-        !isset($input['description']) ||
-        !isset($input['amount']) ||
-        !isset($input['type']) ||
-        !isset($input['transaction_date'])
-    ) {
-        http_response_code(400); // 400 Bad Request - Dados inválidos
+        if (
+            !isset($input['description']) ||
+            !isset($input['amount']) ||
+            !isset($input['type']) ||
+            !isset($input['category']) ||
+            !isset($input['transaction_date'])
+        ) {
+            http_response_code(400); // 400 Bad Request - Dados inválidos
 
-        echo json_encode([
-            'message' => 'Todos os campos sao obrigatorios.'
-        ]);
+            echo json_encode([
+                'message' => 'Todos os campos sao obrigatorios.'
+            ]);
 
-        exit;
-    }
+            exit;
+        }
 
-    $description = $input['description'];
-    $amount = $input['amount'];
-    $type = $input['type'];
-    $transaction_date = $input['transaction_date'];
+        $description = $input['description'];
+        $amount = $input['amount'];
+        $type = $input['type'];
+        $category = $input['category'];
+        $transaction_date = $input['transaction_date'];
 
-    if (trim($description) === '') { //remove espaços em branco do começo e do final
-        http_response_code(400);
+        if (trim($description) === '') { //remove espaços em branco do começo e do final
+            http_response_code(400);
 
-        echo json_encode([
-            'message' => 'Digite uma descricao válida.'
-        ]);
+            echo json_encode([
+                'message' => 'Digite uma descricao válida.'
+            ]);
 
-        exit;
-    }
-    
-    if(strlen($description) < 4){
-        http_response_code(400);
+            exit;
+        }
+        
+        if(strlen($description) < 4){
+            http_response_code(400);
 
-        echo json_encode([
-            'message' => 'Digite uma descricao mais que 4 caracteres.'
-        ]);
+            echo json_encode([
+                'message' => 'Digite uma descricao com pelo menos 4 caracteres.'
+            ]);
 
-        exit;
-    }
+            exit;
+        }
 
-    if($amount <= 0){
-        http_response_code(400);
+        if($amount <= 0){
+            http_response_code(400);
 
-        echo json_encode([
-            'message' => 'Digite um valor valido'
-        ]);
+            echo json_encode([
+                'message' => 'Digite um valor valido'
+            ]);
 
-        exit;
-    }
+            exit;
+        }
 
-    if($type != "entrada" && $type != "saida"){
-        http_response_code(400);
+        if($type != "entrada" && $type != "saida"){
+            http_response_code(400);
 
-        echo json_encode([
-            'message' => 'Digite um tipo valido (entrada/saida)'
-        ]);
+            echo json_encode([
+                'message' => 'Digite um tipo valido (entrada/saida)'
+            ]);
 
-        exit;
-    }
+            exit;
+        }
 
-    $date = DateTime::createFromFormat('Y-m-d', $transaction_date);
+        if (trim($category) === '') { //remove espaços em branco do começo e do final
+            http_response_code(400);
 
-    if (!$date || $date->format('Y-m-d') !== $transaction_date) {
-        http_response_code(400);
+            echo json_encode([
+                'message' => 'Digite uma categoria válida.'
+            ]);
 
-        echo json_encode([
-            'message' => 'Digite uma data valida no formato Ano-Mes-Dia.'
-        ]);
+            exit;
+        }
+        
+        if(strlen($category) < 4){
+            http_response_code(400);
 
-        exit;
-    }
+            echo json_encode([
+                'message' => 'Digite uma categoria com pelo menos 4 caracteres.'
+            ]);
 
-    $stmt = $pdo->prepare(
-        "INSERT INTO transactions (description, amount, type, transaction_date)
-        VALUES (:description, :amount, :type, :transaction_date)"
-    );
+            exit;
+        }
 
-    $stmt->execute([
-        'description' => $description,
-        'amount' => $amount,
-        'type' => $type,
-        'transaction_date' => $transaction_date
-    ]);
+        $date = DateTime::createFromFormat('Y-m-d', $transaction_date);
 
-    http_response_code(201);
+        if (!$date || $date->format('Y-m-d') !== $transaction_date) {
+            http_response_code(400);
 
-    echo json_encode([
-        'message' => 'Novo registro cadastrado com sucesso.'
-    ]);
+            echo json_encode([
+                'message' => 'Digite uma data valida no formato Ano-Mes-Dia.'
+            ]);
 
-    exit;
+            exit;
+        }
 
-
-
-
-
-    //e se tiver 1 milhão de registros
-} else if($method == 'GET'){
-    if (!isset($_GET['id'])) {
         $stmt = $pdo->prepare(
-            "SELECT id, description, amount, type, transaction_date, created_at
-            FROM transactions"
+            "INSERT INTO transactions (description, amount, type, category, transaction_date)
+            VALUES (:description, :amount, :type, :category, :transaction_date)"
         );
 
-        $stmt->execute();
+        $stmt->execute([
+            'description' => $description,
+            'amount' => $amount,
+            'type' => $type,
+            'category' => $category,
+            'transaction_date' => $transaction_date
+        ]);
 
-        $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        http_response_code(201);
 
-        http_response_code(200);
+        echo json_encode([
+            'message' => 'Novo registro cadastrado com sucesso.'
+        ]);
+        break;
 
-        echo json_encode($transactions, JSON_PRETTY_PRINT);
+    case 'GET':
+        $filter = '';
 
-        exit;
+        if (isset($_GET['type'])) {
+            $filter = 'type';
 
-    } else {
+        } elseif (isset($_GET['category'])) {
+            $filter = 'category';
+        }
+
+        switch ($filter) {
+
+            case 'type':
+                $type = $_GET['type'];
+
+                if ($type != "entrada" && $type != "saida") {
+                    http_response_code(400);
+
+                    echo json_encode([
+                        'message' => 'Digite um tipo valido (entrada/saida).'
+                    ]);
+
+                    exit;
+                }
+
+                $stmt = $pdo->prepare(
+                    "SELECT id, description, amount, type, category, transaction_date, created_at
+                    FROM transactions
+                        WHERE type = :type
+                    ORDER BY created_at DESC
+                    LIMIT 5"
+                );
+
+                $stmt->execute([
+                    'type' => $type
+                ]);
+
+                $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                http_response_code(200);
+
+                echo json_encode($transactions, JSON_PRETTY_PRINT);
+
+                break;
+
+            case 'category':
+                $category = $_GET['category'];
+
+
+                $stmt = $pdo->prepare(
+                    "SELECT id, description, amount, type, category, transaction_date, created_at
+                    FROM transactions
+                        WHERE category = :category
+                    ORDER BY created_at DESC
+                    LIMIT 5"
+                );
+
+                $stmt->execute([
+                    'category' => $category
+                ]);
+
+                $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                http_response_code(200);
+
+                echo json_encode($transactions, JSON_PRETTY_PRINT);
+
+                break;
+
+            default:
+                $stmt = $pdo->prepare(
+                    "SELECT id, description, amount, type, category, transaction_date, created_at
+                    FROM transactions
+                    ORDER BY created_at DESC
+                    LIMIT 5"
+                );
+
+                $stmt->execute();
+
+                $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                http_response_code(200);
+
+                echo json_encode($transactions, JSON_PRETTY_PRINT);
+
+                break;
+        }
+
+        break;
+
+    case 'PUT':
+            if (!isset($_GET['id'])) {
+                http_response_code(400);
+
+                echo json_encode([
+                    'message' => 'ID do registro e obrigatorio.'
+                ]);
+
+                exit;
+            }
+
+            $input = json_decode(file_get_contents("php://input"), true);
+
+            if (
+                !isset($input['description']) ||
+                !isset($input['amount']) ||
+                !isset($input['type']) ||
+                !isset($input['category']) ||
+                !isset($input['transaction_date'])
+            ) {
+            
+                http_response_code(400);
+
+                echo json_encode([
+                    'message' => 'Todas as informacoes sao obrigatorias.'
+                ]);
+
+                exit;
+            }
+
+            $description = $input['description'];
+            $amount = $input['amount'];
+            $type = $input['type'];
+            $category = $input['category'];
+            $transaction_date = $input['transaction_date'];
+
+            if (trim($description) === '') { //remove espaços em branco do começo e do final
+                http_response_code(400);
+
+                echo json_encode([
+                    'message' => 'Digite uma descricao válida.'
+                ]);
+
+                exit;
+            }
+            
+            if(strlen($description) < 4){
+                http_response_code(400);
+
+                echo json_encode([
+                    'message' => 'Digite uma descricao com pelo menos 4 caracteres.'
+                ]);
+
+                exit;
+            }
+
+            if($amount <= 0){
+                http_response_code(400);
+
+                echo json_encode([
+                    'message' => 'Digite um valor valido'
+                ]);
+
+                exit;
+            }
+
+            if($type != "entrada" && $type != "saida"){
+                http_response_code(400);
+
+                echo json_encode([
+                    'message' => 'Digite um tipo valido (entrada/saida)'
+                ]);
+
+                exit;
+            }
+
+        if (trim($category) === '') { //remove espaços em branco do começo e do final
+            http_response_code(400);
+
+            echo json_encode([
+                'message' => 'Digite uma categoria válida.'
+            ]);
+
+            exit;
+        }
+        
+        if(strlen($category) < 4){
+            http_response_code(400);
+
+            echo json_encode([
+                'message' => 'Digite uma categoria com pelo menos 4 caracteres.'
+            ]);
+
+            exit;
+        }
+
+        $date = DateTime::createFromFormat('Y-m-d', $transaction_date);
+
+        if (!$date || $date->format('Y-m-d') !== $transaction_date) {
+            http_response_code(400);
+
+            echo json_encode([
+                'message' => 'Digite uma data valida no formato Ano-Mes-Dia.'
+            ]);
+
+            exit;
+        }
+
         $id = $_GET['id'];
 
         $stmt = $pdo->prepare(
-            "SELECT id, description, amount, type, transaction_date, created_at
+            "SELECT id
             FROM transactions
             WHERE id = :id"
         );
@@ -135,213 +333,102 @@ if($method == 'POST'){
         ]);
 
         $transactions = $stmt->fetch(PDO::FETCH_ASSOC);
-    }
 
-    if (!$transactions) {
-        http_response_code(404); // Não encontrado
 
-        echo json_encode([
-            'message' => 'Registro nao encontrado.'
+        if (!$transactions) {
+            http_response_code(404); // Não encontrado
+
+            echo json_encode([
+                'message' => 'Registro nao encontrado.'
+            ]);
+
+            exit;
+        }
+
+        $stmt = $pdo->prepare(
+            "UPDATE transactions
+            SET description = :description,
+                amount = :amount,
+                type = :type,
+                category = :category,
+                transaction_date = :transaction_date
+            WHERE id = :id"
+        );
+
+        $stmt->execute([
+            'description' => $description,
+            'amount' => $amount,
+            'type' => $type,
+            'category' => $category,
+            'transaction_date' => $transaction_date,
+            'id' => $id
         ]);
 
-        exit;
-    }
 
-    http_response_code(200);
-
-    echo json_encode($transactions, JSON_PRETTY_PRINT);
-
-    exit;
-
-
-
-
-
-
-} else if($method == 'PUT'){
-    if (!isset($_GET['id'])) {
-        http_response_code(400);
+        http_response_code(200);
 
         echo json_encode([
-            'message' => 'ID do registro e obrigatorio.'
+            'message' => 'Transacao atualizada com sucesso.'
         ]);
 
-        exit;
-    }
+        break;
 
-    $input = json_decode(file_get_contents("php://input"), true);
+    case 'DELETE':
+        if (!isset($_GET['id'])) {
+            http_response_code(400);
 
-    if (
-        !isset($input['description']) ||
-        !isset($input['amount']) ||
-        !isset($input['type']) ||
-        !isset($input['transaction_date']) 
-    ) {
-    
-        http_response_code(400);
+            echo json_encode([
+                'message' => 'ID da transacao e obrigatorio.'
+            ]);
+
+            exit;
+        }
+
+
+        $id = $_GET['id'];
+
+        $stmt = $pdo->prepare(
+            "SELECT id
+            FROM transactions
+            WHERE id = :id"
+        );
+
+        $stmt->execute([
+            'id' => $id
+        ]);
+
+        $transactions = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$transactions) {
+            http_response_code(404);
+
+            echo json_encode([
+                'message' => 'Transacao nao encontrada.'
+            ]);
+
+            exit;
+        }
+
+        $stmt = $pdo->prepare(
+            "DELETE FROM transactions
+            WHERE id = :id"
+        );
+
+        $stmt->execute([
+            'id' => $id
+        ]);
+
+        http_response_code(200);
 
         echo json_encode([
-            'message' => 'Todas as informacoes sao obrigatorias.'
-        ]);
+            'message' => 'Transacao excluida com sucesso.'
+        ]);        
 
-        exit;
-    }
+        break;
 
-    $description = $input['description'];
-    $amount = $input['amount'];
-    $type = $input['type'];
-    $transaction_date = $input['transaction_date'];
-
-    if (trim($description) === '') { //remove espaços em branco do começo e do final
-        http_response_code(400);
-
+    default:
+        http_response_code(405);
         echo json_encode([
-            'message' => 'Digite uma descricao válida.'
+            'message' => 'Método não permitido.'
         ]);
-
-        exit;
-    }
-    
-    if(strlen($description) < 4){
-        http_response_code(400);
-
-        echo json_encode([
-            'message' => 'Digite uma descricao mais que 4 caracteres.'
-        ]);
-
-        exit;
-    }
-
-    if($amount <= 0){
-        http_response_code(400);
-
-        echo json_encode([
-            'message' => 'Digite um valor valido'
-        ]);
-
-        exit;
-    }
-
-    if($type != "entrada" && $type != "saida"){
-        http_response_code(400);
-
-        echo json_encode([
-            'message' => 'Digite um tipo valido (entrada/saida)'
-        ]);
-
-        exit;
-    }
-
-    $date = DateTime::createFromFormat('Y-m-d', $transaction_date);
-
-    if (!$date || $date->format('Y-m-d') !== $transaction_date) {
-        http_response_code(400);
-
-        echo json_encode([
-            'message' => 'Digite uma data valida no formato Ano-Mes-Dia.'
-        ]);
-
-        exit;
-    }
-
-    $id = $_GET['id'];
-
-    $stmt = $pdo->prepare(
-        "SELECT id
-        FROM transactions
-        WHERE id = :id"
-    );
-
-    $stmt->execute([
-        'id' => $id
-    ]);
-
-    $transactions = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-    if (!$transactions) {
-        http_response_code(404); // Não encontrado
-
-        echo json_encode([
-            'message' => 'Registro nao encontrado.'
-        ]);
-
-        exit;
-    }
-
-    $stmt = $pdo->prepare(
-        "UPDATE transactions
-        SET description = :description,
-            amount = :amount,
-            type = :type,
-            transaction_date = :transaction_date
-        WHERE id = :id"
-    );
-
-    $stmt->execute([
-        'description' => $description,
-        'amount' => $amount,
-        'type' => $type,
-        'transaction_date' => $transaction_date,
-        'id' => $id
-    ]);
-
-
-    http_response_code(200);
-
-    echo json_encode([
-        'message' => 'Transacao atualizada com sucesso.'
-    ]);
-
-} else if($method == 'DELETE'){
-
-    if (!isset($_GET['id'])) {
-        http_response_code(400);
-
-        echo json_encode([
-            'message' => 'ID da transacao e obrigatorio.'
-        ]);
-
-        exit;
-    }
-
-
-    $id = $_GET['id'];
-
-    $stmt = $pdo->prepare(
-        "SELECT id
-        FROM transactions
-        WHERE id = :id"
-    );
-
-    $stmt->execute([
-        'id' => $id
-    ]);
-
-    $transactions = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (!$transactions) {
-        http_response_code(404);
-
-        echo json_encode([
-            'message' => 'Transacao nao encontrada.'
-        ]);
-
-        exit;
-    }
-
-    $stmt = $pdo->prepare(
-        "DELETE FROM transactions
-        WHERE id = :id"
-    );
-
-    $stmt->execute([
-        'id' => $id
-    ]);
-
-    http_response_code(200);
-
-    echo json_encode([
-        'message' => 'Transacao excluida com sucesso.'
-    ]);
 }
